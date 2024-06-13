65123fd8e114b7baf1ebd52dc6f8d8552d8f090d
Irina Silvestrova <lissizza@gmail.com>
2024-06-13 16:44:07 +0500

    improvements

--- discordInteractions.js
+++ discordInteractions.js
@@ -1,8 +1,5 @@
 const { joinVoice, leaveVoice } = require('./audioProcessing');
-const {
-    WHISPER_SETTINGS,
-    AUDIO_SETTINGS,
-} = require('./config');
+const { WHISPER_SETTINGS, AUDIO_SETTINGS } = require('./config');
 const {
     createSettingsModal,
     getSettingsValue,
@@ -22,34 +19,9 @@ async function handleInteractionCreate(interaction) {
 
     try {
         if (interaction.isButton()) {
-            if (interaction.customId === 'join') {
-                await handleJoin(interaction);
-            } else if (interaction.customId === 'leave') {
-                await handleLeave(interaction);
-            } else if (interaction.customId === 'change_channel') {
-                await showChannelSelectionMenu(interaction);
-            } else if (interaction.customId === 'change_user') {
-                await showUserSelectionMenu(interaction);
-            } else if (interaction.customId === 'settings') {
-                await showSettings(interaction);
-            } else if (interaction.customId.startsWith('update_')) {
-                const setting = interaction.customId.substring(7);
-                console.log(`Received update request for setting: ${setting}`);
-                const defaultValue = getSettingsValue(setting);
-                console.log(`Updating setting ${setting} with default value: ${defaultValue}`);
-                if (!defaultValue) {
-                    console.error(`No default value for setting ${setting}`);
-                } else {
-                    const modal = createSettingsModal(setting, defaultValue);
-                    await interaction.showModal(modal);
-                }
-            }
+            await handleButtonInteraction(interaction);
         } else if (interaction.isStringSelectMenu()) {
-            if (interaction.customId === 'select_channel') {
-                await handleChannelSelection(interaction);
-            } else if (interaction.customId === 'select_user') {
-                await handleUserSelection(interaction);
-            }
+            await handleSelectMenuInteraction(interaction);
         } else if (interaction.isModalSubmit()) {
             const setting = interaction.customId.substring(9);
             const newValue = interaction.fields.getTextInputValue(`input_${setting}`);
@@ -63,10 +35,42 @@ async function handleInteractionCreate(interaction) {
     }
 }
 
+async function handleButtonInteraction(interaction) {
+    if (interaction.customId === 'join') {
+        await handleJoin(interaction);
+    } else if (interaction.customId === 'leave') {
+        await handleLeave(interaction);
+    } else if (interaction.customId === 'change_channel') {
+        await showChannelSelectionMenu(interaction);
+    } else if (interaction.customId === 'change_user') {
+        await showUserSelectionMenu(interaction);
+    } else if (interaction.customId === 'settings') {
+        await showSettings(interaction);
+    } else if (interaction.customId.startsWith('update_')) {
+        const setting = interaction.customId.substring(7);
+        console.log(`Received update request for setting: ${setting}`);
+        const defaultValue = getSettingsValue(setting);
+        console.log(`Updating setting ${setting} with default value: ${defaultValue}`);
+        if (!defaultValue) {
+            console.error(`No default value for setting ${setting}`);
+        } else {
+            const modal = createSettingsModal(setting, defaultValue);
+            await interaction.showModal(modal);
+        }
+    }
+}
+
+async function handleSelectMenuInteraction(interaction) {
+    if (interaction.customId === 'select_channel') {
+        await handleChannelSelection(interaction);
+    } else if (interaction.customId === 'select_user') {
+        await handleUserSelection(interaction);
+    }
+}
+
 async function updateSettings(interaction, setting, newValue) {
     console.log(`Received new value for setting ${setting}: ${newValue}`);
 
-    // Update the settings based on the interaction
     if (AUDIO_SETTINGS.hasOwnProperty(setting)) {
         AUDIO_SETTINGS[setting] = parseFloat(newValue);
     } else if (WHISPER_SETTINGS.hasOwnProperty(setting)) {
@@ -139,32 +143,36 @@ async function handleMessageCreate(message) {
     } else if (message.content === '!leave') {
         await handleLeaveCommand(message);
     } else if (message.content.startsWith('!transcribe')) {
-        const args = message.content.split(' ');
-        const target = args[1];
-
-        if (target.startsWith('<#')) {
-            const channelId = target.slice(2, -1);
-            const textChannel = message.guild.channels.cache.get(channelId);
-            if (textChannel) {
-                selectedTextChannels.push({ type: 'channel', value: textChannel });
-                message.reply(`Selected channel for transcription: ${textChannel.name}`);
-            } else {
-                message.reply('Channel not found.');
-            }
-        } else if (target.startsWith('<@')) {
-            const userId = target.slice(2, -1);
-            const user = await message.client.users.fetch(userId);
-            if (user) {
-                selectedTextChannels.push({ type: 'user', value: user });
-                message.reply(`Selected user for transcription: ${user.username}`);
-            } else {
-                message.reply('User not found.');
-            }
-        } else {
-            message.reply('Invalid target for transcription.');
-        }
+        await handleTranscribeCommand(message);
     } else if (message.content === '!settings') {
         await showSettings(message);
+    }
+}
+
+async function handleTranscribeCommand(message) {
+    const args = message.content.split(' ');
+    const target = args[1];
+
+    if (target.startsWith('<#')) {
+        const channelId = target.slice(2, -1);
+        const textChannel = message.guild.channels.cache.get(channelId);
+        if (textChannel) {
+            selectedTextChannels.push({ type: 'channel', value: textChannel });
+            message.reply(`Selected channel for transcription: ${textChannel.name}`);
+        } else {
+            message.reply('Channel not found.');
+        }
+    } else if (target.startsWith('<@')) {
+        const userId = target.slice(2, -1);
+        const user = await message.client.users.fetch(userId);
+        if (user) {
+            selectedTextChannels.push({ type: 'user', value: user });
+            message.reply(`Selected user for transcription: ${user.username}`);
+        } else {
+            message.reply('User not found.');
+        }
+    } else {
+        message.reply('Invalid target for transcription.');
     }
 }
 
